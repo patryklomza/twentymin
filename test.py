2fda5ce12b4dbee42223cbcce1088a38260ab680
Patryk Lomza <p.lomza@outlook.com>
2020-05-16 16:33:26 +0200

    Fix missing imports in functional tests

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import socket
 from urllib.parse import urlparse
+import time
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import override_settings, tag
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 
 
 @tag("selenium")
