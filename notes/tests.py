517949069feede4c06271edbeeb7a94c6a92206b
Patryk Lomza <p.lomza@outlook.com>
2020-05-16 16:26:14 +0200

    Refactor home page unit test to use django client

--- notes/tests.py
+++ notes/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from notes.views import home_page
 
@@ -8,14 +9,6 @@ from notes.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page(self):
-        found = resolve("/")
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode("utf8")
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<h1>Community notes</h1>", html)
-        self.assertTrue(html.endswith("</html>"))
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
