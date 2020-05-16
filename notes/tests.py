1df874a5374c7e332e27175b296fa459e80a9cc1
Patryk Lomza <p.lomza@outlook.com>
2020-05-16 14:14:57 +0200

    Update view and unit tests
    Now the view can be tested in selenium headless browser

--- notes/tests.py
+++ notes/tests.py
@@ -1,11 +1,21 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from notes.views import home_page
 
 # Create your tests here.
 
-class HomePageTest(TestCase):
 
+class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page(self):
-        found = resolve('/')
+        found = resolve("/")
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode("utf8")
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn("<h1>Community notes</h1>", html)
+        self.assertTrue(html.endswith("</html>"))
