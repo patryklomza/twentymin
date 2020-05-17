9408eb07d444d59067a7270440a5916d73ba79a4
Patryk Lomza <p.lomza@outlook.com>
2020-05-17 15:01:10 +0200

    Add a tag for unit tests to call it separately

--- notes/tests.py
+++ notes/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, tag
 from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
@@ -8,6 +8,7 @@ from notes.views import home_page
 # Create your tests here.
 
 
+@tag("unit_test")
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get("/")
