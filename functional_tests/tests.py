4c897078fbdcf0cf0f43d2d29872c9d868a6b1e3
Patryk Łomża <p.lomza@outlook.com>
2020-06-04 19:22:05 +0200

    Update max_wait time to 5

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import WebDriverException
 
-MAX_WAIT = 10
+MAX_WAIT = 5
 
 
 @tag("selenium")
