f1268149deeaf8f9749d0578648363b825a36759
Patryk Łomża <p.lomza@outlook.com>
2020-06-12 19:45:57 +0200

    Add first step to layout FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -149,3 +149,24 @@ class WebTest(BaseTestCase):
         self.assertIn("What a boring book", page_text)
 
         # Satisfied, they both go to sleep
+
+    def test_layout_and_styling(self):
+        # Surma goes to the home page
+        self.selenium.get(self.live_server_url)
+        self.selenium.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.selenium.find_element_by_id("id_new_note")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10
+        )
+
+        # She starts a new list and sees the input is nicely centered there too
+
+        inputbox.send_keys("testing")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_paragraph("testing", self.selenium)
+        inputbox = self.selenium.find_element_by_id("id_new_note")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10
+        )
